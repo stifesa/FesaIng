86da32dd050da87b447d1766ecd38014ebe8258e
Alonso <soportetecnicoingenieria@ferreyros.com.pe>
2024-03-03 23:55:04 -0500

    smarty_data_11

--- dags/DAG_poc00001_smarty_data.py
+++ dags/DAG_poc00001_smarty_data.py
@@ -95,7 +95,7 @@ with DAG(nameDAG,
          default_args = default_args,
          catchup = False,  # Ver caso catchup = True
          max_active_runs = 3,
-         schedule_interval = "30 * * * *") as dag: # schedule_interval = None # Caso sin trigger automático | schedule_interval = "0 12 * * *" | "0,2 12 * * *"
+         schedule_interval = "30 1/6 * * *") as dag: # schedule_interval = None # Caso sin trigger automático | schedule_interval = "0 12 * * *" | "0,2 12 * * *"
 
     # FUENTE: CRONTRAB: https://crontab.guru/
     #############################################################
