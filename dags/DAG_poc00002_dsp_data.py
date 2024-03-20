a31175605caae3e920e80f7b25a6eff2e849a0d0
Alonso <soportetecnicoingenieria@ferreyros.com.pe>
2024-03-20 08:45:08 -0500

    dsp_data_75

--- dags/DAG_poc00002_dsp_data.py
+++ dags/DAG_poc00002_dsp_data.py
@@ -188,7 +188,7 @@ def dsp_load_data(**kwargs):
     quality.reset_index(inplace=True, drop=True)
     # Remove square brackets from the 'col1' column
     #quality['AccionCorrectiva'] = quality['AccionCorrectiva'].str.replace('[', '').str.replace(']', '')
-    print(quality[quality['AccionCorrectiva']])
+    print(quality['AccionCorrectiva'])
     correctivos = quality[quality['AccionCorrectiva'].apply(lambda x: isinstance(x, str) and x != '[]' and x != 'nan')]
     print(correctivos.head())
     print(quality.head(2))
